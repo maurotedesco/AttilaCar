ccb6eec3e87db8ae8a0a504ffad54dee4a4d6235
Mauro Tedesco <mauro.tedesco@linuxservizi.com>
2017-04-27 23:18:11 +0200

    Bluetooth versione 1.1

---
+++ bluetooth_car_Obstacle_v1.1/bluetooth_car_Obstacle_v1.1.ino
@@ -0,0 +1,204 @@
+//www.elegoo.com
+//2016.09.19
+// Modify by Mauro for insert control by ping
+// Versione 1.0 del 28 4 2017
+#include <Servo.h> //servo library
+Servo myservo; // create servo object to control servo
+int Echo = A4;  
+int Trig = A5; 
+int LED=13;
+volatile int state = LOW;
+char getstr;
+int in1=6;
+int in2=7;
+int in3=8;
+int in4=9;
+int ENA=5;
+int ENB=11;
+int ABS=150;
+int rightDistance = 0,leftDistance = 0,middleDistance = 0 ;
+void _mForward()
+{ 
+  digitalWrite(ENA,HIGH);
+  digitalWrite(ENB,HIGH);
+  digitalWrite(in1,HIGH);//digital output
+  digitalWrite(in2,LOW);
+  digitalWrite(in3,HIGH);
+  digitalWrite(in4,LOW);
+  Serial.println("Forward");
+}
+void _mBack()
+{
+  digitalWrite(ENA,HIGH);
+  digitalWrite(ENB,HIGH);
+  digitalWrite(in1,LOW);
+  digitalWrite(in2,HIGH);
+  digitalWrite(in3,LOW);
+  digitalWrite(in4,HIGH);
+  Serial.println("Back");
+}
+void _mright()
+{
+  analogWrite(ENA,ABS);
+  analogWrite(ENB,ABS);
+  digitalWrite(in1,HIGH);
+  digitalWrite(in2,LOW);
+  digitalWrite(in3,LOW);
+  digitalWrite(in4,HIGH); 
+  Serial.println("go right!");
+}
+void _mleft()
+{
+  analogWrite(ENA,ABS);
+  analogWrite(ENB,ABS);
+  digitalWrite(in1,LOW);
+  digitalWrite(in2,HIGH);
+  digitalWrite(in3,HIGH);
+  digitalWrite(in4,LOW);
+  Serial.println("go left!");
+}
+void _mStop()
+{
+  digitalWrite(ENA,LOW);
+  digitalWrite(ENB,LOW);
+  Serial.println("Stop!");
+}
+void stateChange()
+{
+  state = !state;
+  digitalWrite(LED, state);  
+}
+
+/*Ultrasonic distance measurement Sub function*/
+int Distance_test()   
+{
+  digitalWrite(Trig, LOW);   
+  delayMicroseconds(2);
+  digitalWrite(Trig, HIGH);  
+  delayMicroseconds(20);
+  digitalWrite(Trig, LOW);   
+  float Fdistance = pulseIn(Echo, HIGH);  
+  Fdistance= Fdistance/58;       
+  return (int)Fdistance;
+}  
+
+
+void setup()
+{ 
+  pinMode(LED, OUTPUT);
+  myservo.attach(3);// attach servo on pin 3 to servo object
+  Serial.begin(9600);
+  pinMode(Echo, INPUT);    
+  pinMode(Trig, OUTPUT); 
+  pinMode(in1,OUTPUT);
+  pinMode(in2,OUTPUT);
+  pinMode(in3,OUTPUT);
+  pinMode(in4,OUTPUT);
+  pinMode(ENA,OUTPUT);
+  pinMode(ENB,OUTPUT);
+  _mStop();
+}
+void loop()
+  { 
+
+    myservo.write(90);//setservo position according to scaled value
+    delay(500); 
+    middleDistance = Distance_test();
+    #ifdef send
+    Serial.print("middleDistance=");
+    Serial.println(middleDistance);
+    #endif
+          
+  getstr=Serial.read();
+  if(getstr=='f')
+  { // loop principale
+    myservo.write(90);//setservo position according to scaled value
+    delay(500); 
+    middleDistance = Distance_test();
+    #ifdef send
+    Serial.print("middleDistance=");
+    Serial.println(middleDistance);
+    #endif
+     
+    if(middleDistance<=20)
+    {     
+      _mStop();
+      delay(500);                         
+      myservo.write(5);          
+      delay(1000);      
+      rightDistance = Distance_test();
+
+      #ifdef send
+      Serial.print("rightDistance=");
+      Serial.println(rightDistance);
+      #endif
+
+      delay(500);
+       myservo.write(90);              
+      delay(1000);                                                  
+      myservo.write(180);              
+      delay(1000); 
+      leftDistance = Distance_test();
+
+      #ifdef send
+      Serial.print("leftDistance=");
+      Serial.println(leftDistance);
+      #endif
+
+      delay(500);
+      myservo.write(90);              
+      delay(1000);
+      if(rightDistance>leftDistance)  
+      {
+        _mright();
+        delay(360);
+       }
+       else if(rightDistance<leftDistance)
+       {
+        _mleft();
+        delay(360);
+       }
+       else if((rightDistance<=20)||(leftDistance<=20))
+       {
+        _mBack();
+        delay(180);
+       }
+       else
+       {
+        _mForward();
+         delay(360);
+         _mStop();
+       }
+    }  
+    else
+        _mForward();
+         delay(360); 
+        _mStop();
+  } // fine Ciclo Forward 
+  else if(getstr=='b')
+  {
+    _mBack();
+    delay(400);
+    _mStop();
+  }
+  else if(getstr=='l')
+  {
+    _mleft();
+    delay(400);
+    _mStop();
+  }
+  else if(getstr=='r')
+  {
+    _mright();
+    delay(200);
+    _mStop();
+  }
+  else if(getstr=='s')
+  {
+     _mStop();     
+  }
+  else if(getstr=='A')
+  {
+  stateChange();
+  }
+}
